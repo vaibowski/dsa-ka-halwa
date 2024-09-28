d56c53dcb2b5ebb9f0d1bf651ae5381dbb3a062f
vaibowski <vaibhavvishaliitg@gmail.com>
2024-09-28 23:57:10 +0530

    add minor changes to word ladder and comments

--- graphs/questions/word_ladder.py
+++ graphs/questions/word_ladder.py
@@ -5,24 +5,35 @@ from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
 
+        # return immediately if endWord is not in the wordList
         if endWord not in wordList:
             return 0
-        L = len(beginWord)
-        all_combo_dict = defaultdict(list)
-        for word in wordList:
-            for i in range(L):
-                all_combo_dict[word[:i] + "*" + word[i + 1:]].append(word)
+
+        # eliminate redundant words
+        wordSet = set(wordList)
+        length = len(beginWord)
+
+        # make a pattern dict for each word by replacing 1 char each iteration, each word in wordlist will be added
+        # against the pattern set as key. Time complexity = O(n*m) worst case
+        # n being wordList size, and m being length of words
+        patternDict = defaultdict(list)
+        for word in wordSet:
+            for i in range(length):
+                patternDict[word[:i] + "*" + word[i + 1:]].append(word)
+
+        # use BFS to search for the end word level by level
         queue = deque([(beginWord, 1)])
         visited = set()
         visited.add(beginWord)
         while queue:
-            current_word, level = queue.popleft()
-            for i in range(L):
-                intermediate_word = current_word[:i] + "*" + current_word[i + 1:]
-                for word in all_combo_dict[intermediate_word]:
+            currentWord, steps = queue.popleft()
+            for i in range(length):
+                pattern = currentWord[:i] + "*" + currentWord[i + 1:]
+                for word in patternDict[pattern]:
                     if word == endWord:
-                        return level + 1
+                        return steps + 1
                     if word not in visited:
+                        queue.append((word, steps + 1))
                         visited.add(word)
-                        queue.append((word, level + 1))
+
         return 0
