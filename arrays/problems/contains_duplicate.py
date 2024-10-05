c55edd9a8b464c0a1022867baad5fbeeca85eef2
vaibowski <vaibhavvishaliitg@gmail.com>
2024-10-05 22:07:26 +0530

    add contains duplicate solution

---
+++ arrays/problems/contains_duplicate.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        store = set()
+        for num in nums:
+            if num in store:
+                return True
+            store.add(num)
+
+        return False
