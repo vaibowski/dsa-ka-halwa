a7dd030c22346dc0f95f066980290044d228398a
vaibowski <vaibhavvishaliitg@gmail.com>
2024-10-05 22:15:22 +0530

    add comments to islands and treasures

--- graphs/problems/islands_and_treasures.py
+++ graphs/problems/islands_and_treasures.py
@@ -3,15 +3,20 @@ from typing import List
 
 
 class Solution:
+    # 1. push all treasures to a queue with a distance counter set to zero
+    # 2. pop left elements from queue:
+    # 3. if it is treasure: push its land neighbors to the queue
+    # 4. if it is land: set distance using the distance element(2nd index): take min with previous set value
+    # then push the land's neighbors as well
     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
-        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         m, n = len(grid), len(grid[0])
         queue = deque()
 
         for r in range(m):
             for c in range(n):
                 if grid[r][c] == 0:
-                    queue.append([r,c,0])
+                    queue.append([r, c, 0])
 
         while queue:
             x, y, d = queue.popleft()
@@ -22,7 +27,6 @@ class Solution:
                 if row < 0 or row == m or col < 0 or col == n or grid[row][col] <= grid[x][y]:
                     continue
                 else:
-                    queue.append([row, col, d+1])
+                    queue.append([row, col, d + 1])
 
         return
-
