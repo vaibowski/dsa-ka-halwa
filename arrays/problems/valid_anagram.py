86090417eda523a215e21a86a97f478d179dd117
vaibowski <vaibhavvishaliitg@gmail.com>
2024-10-05 22:33:38 +0530

    add valid anagram solution

---
+++ arrays/problems/valid_anagram.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = defaultdict(int)
+        for x in s:
+            count[x] += 1
+
+        for x in t:
+            count[x] -= 1
+
+        for val in count.values():
+            if val != 0:
+                return False
+
+        return True
