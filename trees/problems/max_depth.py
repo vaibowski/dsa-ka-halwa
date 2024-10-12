7b556db350f7d44b0012cff11661cf733c643268
vaibowski <vaibhavvishaliitg@gmail.com>
2024-10-12 22:38:37 +0530

    add max depth of a tree solution

---
+++ trees/problems/max_depth.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return 0
+            if not node.left and not node.right:
+                return 1
+            return max(dfs(node.left), dfs(node.right)) + 1
+
+        return dfs(root)
