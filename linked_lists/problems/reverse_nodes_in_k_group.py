a6d91738dd0417fcadcd35fee0a9147e8ca3045f
vaibowski <vaibhavvishaliitg@gmail.com>
2024-11-11 20:35:01 +0530

    add recursive reverse node in k group linked lists solution

--- linked_lists/problems/reverse_nodes_in_k_group.py
+++ linked_lists/problems/reverse_nodes_in_k_group.py
@@ -4,7 +4,8 @@ from linked_lists.problems.merge_two_sorted_lists import ListNode
 
 
 class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    # recursive solution is the best btw
+    def reverseKGroupIterative(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if k == 1 or head is None:
             return head
         dummy = ListNode(0, head)
@@ -34,3 +35,26 @@ class Solution:
             cur = cur.next
             k -= 1
         return cur
+
+    def reverseKGroupRecursive(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Check if we need to reverse the group
+        cur = head
+        for _ in range(k):
+            if not cur:
+                return head
+            cur = cur.next
+
+        # Reverse the group (basic way to reverse linked list)
+        prev = None
+        cur = head
+        for _ in range(k):
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        # After reverse, we know that `head` is the tail of the group.
+        # And `curr` is the next pointer in original linked list order
+        head.next = self.reverseKGroup(cur, k)
+        return prev
+
