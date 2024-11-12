84f58cec1d60d937c00a8ca1a0de94d5c3665cb5
vaibowski <vaibhavvishaliitg@gmail.com>
2024-11-12 13:29:57 +0530

    add is subtree of another tree solution

---
+++ trees/problems/subtree_of_another_tree.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from trees.problems.max_depth import TreeNode
+
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if root is None and subRoot is None:
+            return True
+        if root is None:
+            return False
+        if root.val == subRoot.val:
+            if self.isSameTree(root, subRoot):
+                return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSameTree(self, p, q):
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
