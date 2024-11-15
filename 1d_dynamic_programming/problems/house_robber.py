108fd6dab000d67bd3b5b536bd3b8bed3646b43c
vaibowski <vaibhavvishaliitg@gmail.com>
2024-11-15 19:27:13 +0530

    add house robber constant space solution

--- 1d_dynamic_programming/problems/house_robber.py
+++ 1d_dynamic_programming/problems/house_robber.py
@@ -8,6 +8,16 @@ def rob(self, nums: List[int]) -> int:
     dp = [*nums]
     dp[1] = max(nums[0], nums[1])
     for i in range(2, len(nums)):
-        dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
 
     return dp[-1]
+
+
+# algorithm to solve this in constant space
+def robOptimized(self, nums: List[int]) -> int:
+    prev, cur = 0, 0
+    for num in nums:
+        temp = prev
+        prev = cur
+        cur = max(prev, num + temp)
+    return cur
