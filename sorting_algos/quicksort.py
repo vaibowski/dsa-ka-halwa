91b744745adc7b8bab3f9133822d6e9af4d854d0
vaibowski <vaibhavvishaliitg@gmail.com>
2024-11-18 19:05:57 +0530

    add quicksort solution

---
+++ sorting_algos/quicksort.py
@@ -0,0 +1,27 @@
+from random import random
+from typing import List
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.quickSort(nums, 0, len(nums) - 1)
+        return nums
+
+    def quickSort(self, nums, left, right):
+        if left >= right:
+            return
+
+        pivot = nums[random.randint(left, right)]
+        i, j = left, right
+        while i <= j:
+            while nums[i] < pivot:  # Find first i such that nums[i] >= pivot
+                i += 1
+            while j > left and nums[j] > pivot:  # Find first j such that nums[j] <= pivot
+                j -= 1
+            if i <= j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
+        self.quickSort(nums, left, j)  # nums[left..j] are elements < pivot
+        self.quickSort(nums, i, right)  # nums[i..right] are elements > pivot
