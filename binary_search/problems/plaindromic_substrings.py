443d96274265537240e6ade70b73b81f201af80e
vaibowski <vaibhavvishaliitg@gmail.com>
2024-11-22 18:28:15 +0530

    add palindromic substring solution

---
+++ binary_search/problems/plaindromic_substrings.py
@@ -0,0 +1,17 @@
+def countSubstrings(s: str) -> int:
+    ans = 0
+
+    for i in range(len(s)):
+        left, right = i, i
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            ans += 1
+            left -= 1
+            right += 1
+
+        left, right = i, i + 1
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            ans += 1
+            left -= 1
+            right += 1
+
+    return ans
